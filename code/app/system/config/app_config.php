0cf2e0cf4cc03819448bcd80daeb3bb039d99c75
abolabo <abolabo@gmail.com>
2017-07-12 16:04:16 +0300

    split config + removed trunk

---
+++ code/app/system/config/app_config.php
@@ -0,0 +1,36 @@
+<?php
+// set default encoding for multibyte php mod
+mb_internal_encoding('UTF-8');
+ini_set('default_charset', 'utf-8');
+
+define('INDEX_FILE', 'index.php');
+
+$root_app = realpath(__DIR__.'/../../').'/';
+// Windows IIS Compatibility
+if (stristr(PHP_OS, 'WIN')) {
+	define('IS_WINDOWS', true);
+	$root_app = str_replace('\\', '/', $root_app);
+}
+
+define('DIR_APP', $root_app);
+define('DIR_CORE', DIR_APP . 'core/');
+define('DIR_LIB', DIR_APP . 'lib/');
+//resources
+
+define('DIR_RESOURCE', DIR_ASSETS . 'resources/');
+
+// SEO URL Keyword separator
+define('SEO_URL_SEPARATOR', '-');
+
+// EMAIL REGEXP PATTERN
+define('EMAIL_REGEX_PATTERN','/^[A-Z0-9._%-]+@[A-Z0-9.-]{0,61}[A-Z0-9]\.[A-Z]{2,16}$/i');
+
+//postfixes for template override
+define('POSTFIX_OVERRIDE', '.override');
+define('POSTFIX_PRE', '.pre');
+define('POSTFIX_POST', '.post');
+
+require_once 'database.php';
+
+
+
---
+++ code/app/system/config/database.php
@@ -0,0 +1,8 @@
+<?php
+// Database Configuration
+define('DB_DRIVER', 'amysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'abolabo');
+define('DB_DATABASE', 'abc_2.0');
+define('DB_PREFIX', 'cba_');
