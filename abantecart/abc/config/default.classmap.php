d40dc15a35677724d31552c50e5bb8896c93a730
abolabo <abolabo@gmail.com>
2018-07-12 18:32:00 +0300

    config.classmap changes

--- abantecart/abc/config/default.classmap.php
+++ abantecart/abc/config/default.classmap.php
@@ -4,8 +4,10 @@
  */
 
 use abc\core\engine\AAttribute;
+use abc\core\lib\AAttribute_Manager;
 use abc\core\lib\ACart;
 use abc\core\lib\ACustomer;
+use abc\core\lib\AEncryption;
 use abc\core\lib\AJobManager;
 use abc\core\lib\AJson;
 use abc\core\lib\ALog as ALog;
@@ -17,8 +19,8 @@ use abc\core\lib\AResourceManager;
 use Illuminate\Events\Dispatcher as EventDispatcher;
 
 return [
-    'AViewRender'     => \abc\core\view\AViewDefaultRender::class,
-    'ALog'            => [
+    'AViewRender'        => \abc\core\view\AViewDefaultRender::class,
+    'ALog'               => [
         ALog::class,
         [
 
@@ -28,15 +30,17 @@ return [
             'debug'    => 'debug.log',
         ],
     ],
-    'AResourceManager'=> AResourceManager::class,
-    'ABackup'         => ABackup::class,
-    'AJobManager'     => AJobManager::class,
-    'AJson'           => AJson::class,
-    'ACustomer'       => ACustomer::class,
-    'AAttribute'      => AAttribute::class,
-    'APromotion'      => APromotion::class,
-    'ACart'           => ACart::class,
-    'AOrder'          => AOrder::class,
-    'AOrderManager'   => AOrderManager::class,
-    'EventDispatcher' => [EventDispatcher::class, null],
+    'AResourceManager'   => AResourceManager::class,
+    'ABackup'            => ABackup::class,
+    'AJobManager'        => AJobManager::class,
+    'AJson'              => AJson::class,
+    'ACustomer'          => ACustomer::class,
+    'AAttribute'         => AAttribute::class,
+    'AAttribute_Manager' => AAttribute_Manager::class,
+    'APromotion'         => APromotion::class,
+    'ACart'              => ACart::class,
+    'AOrder'             => AOrder::class,
+    'AOrderManager'      => AOrderManager::class,
+    'EventDispatcher'    => [EventDispatcher::class, null],
+    'AEncryption'        => AEncryption::class,
 ];
