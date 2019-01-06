1855e1e0669c244289ccdefa60416774a126acdc
abantecart <projkov@abantecart.com>
2019-01-06 12:36:28 -0500

    New test architecture

---
+++ abantecart/abc/tests/unit/config/default/model.php
@@ -0,0 +1,31 @@
+<?php
+
+use abc\modules\listeners\ModelAuditListener;
+use abc\tests\unit\modules\listeners\ATestListener;
+
+return [
+    /** events for ORM Models
+     can be
+     eloquent.retrieved
+     eloquent.creating
+     eloquent.created
+     eloquent.updating
+     eloquent.updated
+     eloquent.saving
+     eloquent.saved
+     eloquent.deleting
+     eloquent.deleted
+     eloquent.restoring
+     eloquent.restored
+     * @see more info https://laravel.com/docs/5.6/eloquent#events */
+
+    'EVENTS' => [
+        //call some listeners on model event
+        'eloquent.saved: abc\models\base\Product' => [ ATestListener::class ],
+        //call listeners on every model event
+        'eloquent.*: *' => [
+            //this listener firing by base model property $auditEvents
+            ModelAuditListener::class
+        ],
+    ]
+];
