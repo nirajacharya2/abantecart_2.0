27079aceb177f46dbfd594a0ced1d9ba18c66d22
abolabo <abolabo@gmail.com>
2019-02-01 14:37:57 +0200

    models/catalog/Product fixes

--- abantecart/abc/models/catalog/Product.php
+++ abantecart/abc/models/catalog/Product.php
@@ -4,11 +4,13 @@ namespace abc\models\catalog;
 
 use abc\models\BaseModel;
 use abc\core\engine\AResource;
+use abc\models\order\CouponsProduct;
+use abc\models\order\OrderProduct;
+use abc\models\system\Audit;
 use abc\models\system\Store;
 use Exception;
 use H;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use abc\models\catalog\ProductDescription;
 
 /**
  * Class Product
