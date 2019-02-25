a8525e09f7e52598d74438412c8d42ef8216e30f
denis <vdvpie22@gmail.com>
2019-02-25 15:09:32 +0300

    Fix ac_global_attributes_groups - add timestamps

--- abantecart/install/mysql.database.sql
+++ abantecart/install/mysql.database.sql
@@ -12865,7 +12865,9 @@ CREATE TABLE `ac_global_attributes_groups` (
   `attribute_group_id` 	int(11) NOT NULL AUTO_INCREMENT,
   `sort_order` 			int(3) NOT NULL DEFAULT '0',
   `status` 				smallint(1) NOT NULL default '0',
-
+  `date_added` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_modified` timestamp NULL DEFAULT CURRENT_TIMESTAMP  ON UPDATE CURRENT_TIMESTAMP,
+  `date_deleted` timestamp NULL,
   PRIMARY KEY (`attribute_group_id`)
 ) ENGINE=INNODB  DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci AUTO_INCREMENT=1;
 
