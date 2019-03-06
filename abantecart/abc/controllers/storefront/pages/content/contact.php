b8d97672ed66e2d39297cb3ab323fc8671820226
abolabo <abolabo@gmail.com>
2019-03-06 20:05:46 +0200

    fixes from 1.2.14

--- abantecart/abc/controllers/storefront/pages/content/contact.php
+++ abantecart/abc/controllers/storefront/pages/content/contact.php
@@ -95,13 +95,15 @@ class ControllerPagesContentContact extends AController
             $this->data['mail_template_data']['form_fields'] = [];
             foreach ($form_fields as $field_name => $field_info) {
                 if (H::has_value($post_data[$field_name])
-                    && !in_array($field_name, ['first_name', 'email', 'enquiry', 'captcha'])) {
+                    && !in_array($field_name, ['first_name', 'email', 'enquiry', 'captcha'])
+                ) {
+                    $field_value = $post_data[$field_name];
+                    if(is_array($field_value)){
+                        $field_value = implode("; ",$field_value);
+                    }
                     $field_details = $this->form->getField($field_name);
-                    $this->data['mail_plain_text'] .= "\r\n".rtrim($field_details['name'], ':')
-                                                      .":\t"
-                                                      .$post_data[$field_name];
-                    $this->data['mail_template_data']['form_fields'][rtrim($field_details['name'], ':')] =
-                        $post_data[$field_name];
+                    $this->data['mail_plain_text'] .= "\r\n".rtrim($field_details['name'], ':').":\t".$field_value;
+                    $this->data['mail_template_data']['form_fields'][rtrim($field_details['name'], ':')] = $field_value;
                 }
             }
 
