9cc37b81a89a15d8e5210e57df70c99b70f11385
abolabo <abolabo@gmail.com>
2023-04-14 17:32:18 +0300

    core: migration improvements

--- abantecart/abc/migrations/20230207164957_content_f_k_changes.php
+++ abantecart/abc/migrations/20230207164957_content_f_k_changes.php
@@ -17,24 +17,32 @@ class ContentFKChanges extends AbstractMigration
     public function up()
     {
 
-        $sql = "alter table tims_contents
+        try {
+            $sql = "alter table tims_contents
                     drop constraint tims_contents_tims_contents_content_id_fk;";
-        $this->query($sql);
-        $sql = "alter table tims_contents
+            $this->query($sql);
+        } catch (Exception $e) {
+        }
+        try {
+            $sql = "alter table tims_contents
                     add constraint tims_contents_tims_contents_content_id_fk
                         foreign key (parent_id) references tims_contents (content_id)
                             on update cascade on delete cascade;";
-        $this->query($sql);
+            $this->query($sql);
+        } catch (Exception $e) {
+        }
 
         $sql = "DELETE FROM tims_contents_to_stores 
                 WHERE content_id NOT IN (SELECT content_id FROM tims_contents)";
         $this->query($sql);
-
-        $sql = "alter table tims_contents_to_stores
+        try {
+            $sql = "alter table tims_contents_to_stores
                     add constraint tims_contents_2_stores_id_fk
                         foreign key (content_id) references tims_contents (content_id)
                             on update cascade on delete cascade ;";
-        $this->query($sql);
+            $this->query($sql);
+        } catch (Exception $e) {
+        }
     }
 
     public function down()
