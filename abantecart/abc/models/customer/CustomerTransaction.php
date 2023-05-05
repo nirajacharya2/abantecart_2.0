5880cfec33770649c840b001bbf9a03b518d0ebf
abolabo <abolabo@gmail.com>
2023-05-05 11:06:26 +0300

    Core: init.cli cache register fix

--- abantecart/abc/models/customer/CustomerTransaction.php
+++ abantecart/abc/models/customer/CustomerTransaction.php
@@ -259,16 +259,16 @@ class CustomerTransaction extends BaseModel
     public static function getBalance(int $customer_id, $update = false)
     {
         $customer = Customer::find($customer_id);
-        if(!$customer){
+        if (!$customer) {
             return null;
         }
         $customer->running_balance_datetime = $customer->running_balance_datetime ?: '1970-01-01';
-        if($customer->running_balance_datetime == '1970-01-01'){
+        if ($customer->running_balance_datetime == '1970-01-01') {
             $customer->running_balance = 0;
         }
 
         $customerTransaction = new static;
-        $transTable = $customerTransaction->getConnection()->getTablePrefix().$customerTransaction->getTable();
+        $transTable = $customerTransaction->getConnection()->getTablePrefix() . $customerTransaction->getTable();
         $now = date('Y-m-d H:i:s');
         // do not allow additional filters here! globalScopes must be disabled here
         $query = static::withoutGlobalScopes()
@@ -296,10 +296,10 @@ class CustomerTransaction extends BaseModel
         $aliasC = $db->table_name('customer_transactions');
         $aliasU = $db->table_name('users');
 
-        $rawInc = "CASE WHEN ".$aliasC.".section = 1
-                        THEN CONCAT(".$aliasCu.".firstname,' ',".$aliasCu.".lastname, ' (',".$aliasCu.".loginname,')') 
+        $rawInc = "CASE WHEN " . $aliasC . ".section = 1
+                        THEN CONCAT(" . $aliasCu . ".firstname,' '," . $aliasCu . ".lastname, ' ('," . $aliasCu . ".loginname,')') 
                     ELSE
-                        CONCAT(".$aliasU.".firstname,' ',".$aliasU.".lastname, ' (',".$aliasU.".username,')')
+                        CONCAT(" . $aliasU . ".firstname,' '," . $aliasU . ".lastname, ' ('," . $aliasU . ".username,')')
                     END";
 
         $select = [];
@@ -307,26 +307,26 @@ class CustomerTransaction extends BaseModel
             $select[] = $db->raw('COUNT(*) as total');
         } else {
             $select = [
-                $db->raw($rawInc." as user")
+                $db->raw($rawInc . " as user")
             ];
         }
 
-        $query = CustomerTransaction::selectRaw($db->raw_sql_row_count()." ".$db->table_name('customer_transactions').".*")
-                ->addSelect($select)
-                ->leftJoin(
-                      'users',
-                      'users.user_id',
-                      '=',
-                      'customer_transactions.created_by'
-                  )
-                ->leftJoin(
-                      'customers',
-                      'customers.customer_id',
-                      '=',
-                      'customer_transactions.created_by'
-                  );
-        if($data['customer_id']) {
-            $query->where(['customer_transactions.customer_id' => (int) $data['customer_id']]);
+        $query = CustomerTransaction::selectRaw($db->raw_sql_row_count() . " " . $db->table_name('customer_transactions') . ".*")
+            ->addSelect($select)
+            ->leftJoin(
+                'users',
+                'users.user_id',
+                '=',
+                'customer_transactions.created_by'
+            )
+            ->leftJoin(
+                'customers',
+                'customers.customer_id',
+                '=',
+                'customer_transactions.created_by'
+            );
+        if ($data['customer_id']) {
+            $query->where(['customer_transactions.customer_id' => (int)$data['customer_id']]);
         }
 
         $filter = $data['filter'] ?? [];
@@ -337,22 +337,22 @@ class CustomerTransaction extends BaseModel
                                     AND '" . $db->escape($filter['date_end']) . "'");
         }
         if (H::has_value($filter['debit'])) {
-            $query->whereRaw( "ROUND(".$aliasC.".debit,2) = '".round((float)$filter['debit'], 2)."'" );
+            $query->whereRaw("ROUND(" . $aliasC . ".debit,2) = '" . round((float)$filter['debit'], 2) . "'");
         }
 
         if (H::has_value($filter['credit'])) {
-            $query->whereRaw( "ROUND(".$aliasC.".credit,2) = '".round((float)$filter['credit'], 2)."'" );
+            $query->whereRaw("ROUND(" . $aliasC . ".credit,2) = '" . round((float)$filter['credit'], 2) . "'");
         }
         if (H::has_value($filter['transaction_type'])) {
-            $query->whereRaw( $aliasC.".transaction_type LIKE '%".$db->escape($filter['transaction_type'])."%'");
+            $query->whereRaw($aliasC . ".transaction_type LIKE '%" . $db->escape($filter['transaction_type']) . "%'");
         }
 
         if (H::has_value($filter['description'])) {
-            $query->whereRaw( $aliasC.".description LIKE '%".$db->escape($filter['description'])."%'");
+            $query->whereRaw($aliasC . ".description LIKE '%" . $db->escape($filter['description']) . "%'");
         }
 
         if (H::has_value($filter['user'])) {
-            $query->whereRaw( "LOWER(".$rawInc.") like '%".mb_strtolower($db->escape($filter['user']))."%'");
+            $query->whereRaw("LOWER(" . $rawInc . ") like '%" . mb_strtolower($db->escape($filter['user'])) . "%'");
         }
 
         //If for total, we're done building the query
@@ -364,32 +364,32 @@ class CustomerTransaction extends BaseModel
         }
 
         $sort_data = [
-            'date_added' => 'customer_transactions.date_added',
-            'user' => 'user',
-            'debit' => 'customer_transactions.debit',
-            'credit' => 'customer_transactions.credit',
+            'date_added'       => 'customer_transactions.date_added',
+            'user'             => 'user',
+            'debit'            => 'customer_transactions.debit',
+            'credit'           => 'customer_transactions.credit',
             'transaction_type' => 'customer_transactions.transaction_type',
         ];
 
-         // NOTE: Performance slowdown might be noticed or larger search results
+        // NOTE: Performance slowdown might be noticed or larger search results
 
-         $orderBy = $sort_data[$data['sort']] ? : 'customer_transactions.date_added';
-         if (isset($data['order']) && (strtoupper($data['order']) == 'DESC')) {
-             $sorting = "desc";
-         } else {
-             $sorting = "asc";
-         }
+        $orderBy = $sort_data[$data['sort']] ?: 'customer_transactions.date_added';
+        if (isset($data['order']) && (strtoupper($data['order']) == 'DESC')) {
+            $sorting = "desc";
+        } else {
+            $sorting = "asc";
+        }
 
-         $query->orderBy( $orderBy, $sorting);
-         if (isset($data['start']) || isset($data['limit'])) {
-             if ($data['start'] < 0) {
-                 $data['start'] = 0;
-             }
-             if ($data['limit'] < 1) {
-                 $data['limit'] = 20;
-             }
-             $query->offset((int)$data['start'])->limit((int)$data['limit']);
-         }
+        $query->orderBy($orderBy, $sorting);
+        if (isset($data['start']) || isset($data['limit'])) {
+            if ($data['start'] < 0) {
+                $data['start'] = 0;
+            }
+            if ($data['limit'] < 1) {
+                $data['limit'] = 20;
+            }
+            $query->offset((int)$data['start'])->limit((int)$data['limit']);
+        }
 
         //allow to extend this method from extensions
         Registry::extensions()->hk_extendQuery(new static, __FUNCTION__, $query, $data);
@@ -406,7 +406,7 @@ class CustomerTransaction extends BaseModel
             ->distinct(['transaction_type'])
             ->orderBy('transaction_type');
         //allow to extend this method from extensions
-        Registry::extensions()->hk_extendQuery(new static,__FUNCTION__, $query);
+        Registry::extensions()->hk_extendQuery(new static, __FUNCTION__, $query);
         return $query->useCache('customer')->get();
     }
 
